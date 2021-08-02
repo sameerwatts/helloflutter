d0eb1aa93494b85ed0d966692166478b768b10ff
Sameer Watts <sameerwatts00@gmail.com>
2021-08-02 21:57:39 +0530

    Create first application

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text('Welcome to 30 days of flutter'),
+          ),
+        ),
+      ),
+    );
+  }
+}
